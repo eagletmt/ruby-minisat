31e3e1c86f1b5ff9d5cdeb07b22a67f637dd1905
Kohei Suzuki <eagletmt@gmail.com>
2013-12-29 13:54:39 +0900

    Use $CPPFLAGS

--- ext/minisat/extconf.rb
+++ ext/minisat/extconf.rb
@@ -4,15 +4,7 @@ require 'fileutils'
 top = File.expand_path('../../../vendor/minisat', __FILE__)
 $objs = %W[minisat.o Solver.o]
 $INCFLAGS = "#{$INCFLAGS} -I#{top}"
-
-# XXX: Why mkmf doesn't see $CXXFLAGS ?
-unless defined?(MakeMakefile::CONFIG)
-  # Ruby 1.9
-  module MakeMakefile
-    CONFIG = ::CONFIG
-  end
-end
-MakeMakefile::CONFIG['CXXFLAGS'] += ' -D__STDC_LIMIT_MACROS -D__STDC_FORMAT_MACROS'
+$CPPFLAGS = "#{$CPPFLAGS} -D__STDC_LIMIT_MACROS -D__STDC_FORMAT_MACROS"
 
 FileUtils.ln_s File.join(top, 'core', 'Solver.cc'), '.', force: true
 
