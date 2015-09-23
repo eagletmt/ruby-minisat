fda89d18c2930bedf34ecfcc914b42172fa46ffd
Kohei Suzuki <eagletmt@gmail.com>
2015-09-23 17:25:28 +0900

    Restore CXXFLAGS for some platforms

--- ext/minisat/extconf.rb
+++ ext/minisat/extconf.rb
@@ -4,6 +4,7 @@ require 'fileutils'
 top = File.expand_path('../../../vendor/minisat', __FILE__)
 $srcs = %W[minisat.cc Solver.cc]
 $INCFLAGS = "#{$INCFLAGS} -I#{top}"
+$CPPFLAGS = "#{$CPPFLAGS} -D__STDC_LIMIT_MACROS -D__STDC_FORMAT_MACROS"
 
 FileUtils.ln_s(File.join(top, 'core', 'Solver.cc'), '.', force: true)
 
