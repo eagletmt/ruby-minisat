eb7bc7959ae9fb5b9e3b0b7e03f5db6dc7c4259b
Kohei Suzuki <eagletmt@gmail.com>
2015-09-23 17:22:14 +0900

    Fix link command

--- ext/minisat/extconf.rb
+++ ext/minisat/extconf.rb
@@ -2,12 +2,9 @@ require 'mkmf'
 require 'fileutils'
 
 top = File.expand_path('../../../vendor/minisat', __FILE__)
-$objs = %W[minisat.o Solver.o]
+$srcs = %W[minisat.cc Solver.cc]
 $INCFLAGS = "#{$INCFLAGS} -I#{top}"
-$CPPFLAGS = "#{$CPPFLAGS} -D__STDC_LIMIT_MACROS -D__STDC_FORMAT_MACROS"
 
-FileUtils.ln_s File.join(top, 'core', 'Solver.cc'), '.', force: true
+FileUtils.ln_s(File.join(top, 'core', 'Solver.cc'), '.', force: true)
 
-if have_library 'stdc++'
-  create_makefile 'minisat'
-end
+create_makefile 'minisat'
